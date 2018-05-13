dd2fba1cf6cf9593b5650fd03521d4fc4c0684ce
nikitabhargava <nikita7.bhargava@gmail.com>
2018-05-13 12:49:22 -0400

    Add files via upload

--- query.py
+++ query.py
@@ -11,7 +11,6 @@ data=r.json()
 
 
 #Converting to CSV 
-data = """[{'payloadtimestamp': '1526001268', 'temperature': '101', 'clientid': 'C1', 'bp': '88', 'heartrate': '74'}, {'payloadtimestamp': '1526001273', 'temperature': '102', 'clientid': 'C1', 'bp': '145', 'heartrate': '74'}, {'payloadtimestamp': '1526001278', 'temperature': '100', 'clientid': 'C1', 'bp': '82', 'heartrate': '76'}, {'payloadtimestamp': '1526001283', 'temperature': '101', 'clientid': 'C1', 'bp': '92', 'heartrate': '74'}, {'payloadtimestamp': '1526001288', 'temperature': '102', 'clientid': 'C1', 'bp': '89', 'heartrate': '73'}, {'payloadtimestamp': '1526001293', 'temperature': '96', 'clientid': 'C1', 'bp': '97', 'heartrate': '72'}, {'payloadtimestamp': '1526001298', 'temperature': '98', 'clientid': 'C1', 'bp': '88', 'heartrate': '76'}, {'payloadtimestamp': '1526001303', 'temperature': '100', 'clientid': 'C1', 'bp': '99', 'heartrate': '73'}, {'payloadtimestamp': '1526001308', 'temperature': '99', 'clientid': 'C1', 'bp': '139', 'heartrate': '72'}, {'payloadtimestamp': '1526001313', 'temperature': '98', 'clientid': 'C1', 'bp': '99', 'heartrate': '73'}]"""
 f = csv.writer(open("mlData.csv", "w"))
 f.writerow(["payloadtimestamp", "temperature", "clientid", "bp", "heartrate"])
 
